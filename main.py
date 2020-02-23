06e38e4976720a6fb6391f3a7258c133cd363532
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-02-23 19:42:44 +0100

    get

--- main.py
+++ main.py
@@ -2,59 +2,18 @@ from pymongo import MongoClient
 from flask import Flask, request
 import pymongo
 import json
+import argparse
+import requests
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['eslac']
 
 app = Flask(__name__)
 
-"""### 1. User endpoints
-
-- (GET) `/user/create/<username>`
-  OK - **Purpose:** Create a user and save into DB
-  OK - **Params:** `username` the user name
-  OK - **Returns:** `user_id`
-
-
-- (GET) `/user/<user_id>/recommend`
-  - **Purpose:** Recommend friend to this user based on chat contents
-  - **Returns:** json array with top 3 similar users
-"""
-
-"""
-def make_sure_exists(story, stories_col):
-    data = stories_col.find_one({'story': story})
-    if data is not None:
-        return data.['_id']
-    return stories_col.insert_one({'story': story}).inserted_id
-
-"""
-
-@app.route("/")
-def hello ():
-    return "Add or get info using the endpoints"
-
-@app.route("/test")
-def test ():
-    return "The API works!"
-
-@app.route("/user/create/<name>")
-def createUser (name):
-    nombre = {"Name":f"{name}"}
-    db.users.insert_one(nombre) #Create document within the users collection
-    #users = pymongo.Connection()['users']['name']
-    #_id = collection.insert({"name": "tyler"}) #To retrieve the assigned _id
-    return "The user {} has been created".format(name)
-
-"""
-@app.route("/getid/<name>")
+#GET (GETTING INFORMATION) users & conversations
+@app.route("/getid/<name>") 
 def getUserId(name):
-    item = db.users.find_one({"Name":f"{name}"})
-    numero = item.get('_id')
-    return json.dumps(numero, default=str)
-"""
-@app.route("/getid/<name>")
-def getUserId(name):
+    #Get user's info by inputting their name
     for i in db.users.find({"Name":f"{name}"}):
         ides = i.get('_id')
         return json.dumps(ides, default=str)
@@ -68,19 +27,72 @@ def totalChats():
     total = db.conversations.countDocuments({})
     return total
 
-#def createChat(*users):
- #   return chatID
 
-@app.route("/createchat")
-def createUsersArray (x, y):
-    x = input("Introduce un usuario: ")
-    y = input("Introduce el segundo usuario": )
+#TESTING THE API
+@app.route("/")
+def hello ():
+    return "Add or get info using the endpoints"
 
-    new_chat = [x, y]
-    
-    nombre = ({"Primer usuario":f"{x}"}, {"Segundo usuario":f"{y}"})
-    db.conversations.insert_one(new_chat)
-    return "The user {} has been created".format(name)
+@app.route("/test")
+def test ():
+    return "The API works!"
+
+
+#CREATEEEEEEEEEEEEEEEE (INSERTING COLLECTIONS) users & conversations
+
+@app.route("/user/create/<name>")
+def createUser (name):
+    nombre = {"Name":f"{name}"}
+    users_match = db.users.find({"Name":f"{name}"}, projection={name:True})
+    if len(list(users_match)) > 0:
+        return "That username already exists"
+    else: 
+        db.users.insert_one(nombre) #Creates document within the users collection
+        a = getUserId(name)
+        return "The user {} has been created and added to the DB with {}.".format(name, a)
+
+"""
+@app.route("/user/create/<name>")
+def createUser (name):
+    nombre = {"Name":f"{name}"}
+    db.users.insert_one(nombre) #Create document within the users collection
+    #users = pymongo.Connection()['users']['name']
+    #_id = collection.insert({"name": "tyler"}) #To retrieve the assigned _id
+    return "The user {} has been created and added to the DB.".format(name)
+"""
+"""
+@app.route("/chat/create") 
+def createChatWithTwoPeopleOnJupyter ():
+    #Create a two people conversation on jupyter
+    user1 = request.args.get(key="First user")
+    user2 = request.args.get(key="Second user")
+
+    new_chat = {"First user" : user1, "Second user": user2}
+
+    db.conversations.insert(new_chat) #Create document within the users collection in Mongo
+    return f"{new_chat} has been created."
+
+createUser (name)
+"""
+
+@app.route("/chat/create") 
+def createChatWithTwoPeopleOnJupyter ():
+    #Create a two people conversation on jupyter
+    user1 = request.args.get(key="First user")
+    user2 = request.args.get(key="Second user")
+
+    new_chat = {"Users": {"First user" : user1, "Second user": user2}}
+
+    db.conversations.insert(new_chat) #Create document within the users collection in Mongo
+    return f"{new_chat} has been created."
+
+"""
+@app.route("/chat/<chat_id>/adduser")
+def addUserToChat():
+    #meterle un id
+    add_user_to_chat = db.conversations.find({"_id":f"{chat_id}"}, projection={name:True})
+    returns chat_id
+"""
 
 
 app.run("0.0.0.0", 5000, debug=True)
@@ -93,3 +105,8 @@ max()+1
 
 
 """
+
+
+
+
+
