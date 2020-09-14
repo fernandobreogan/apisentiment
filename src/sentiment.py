21acceb1fbf41727664107cc13d3e12ea69ecdda
breogann <fernandocostapradillo@gmail.com>
2020-09-14 11:25:19 +0200

    spacy

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,6 +1,21 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
+
+
+def checkLanguage (message):
+    b = TextBlob(message)
+    lang = b.detect_language()
+    return lang
+    
+
+def translate (message):
+    if checkLanguage == ('en'):
+        en_blob = TextBlob(message)
+        en_blob=en_blob.translate(from_lang='es',to='en')
+
 
 def messageSentiment(message):
+    checkLanguage(message)
     sia = SentimentIntensityAnalyzer()
     sentiment = sia.polarity_scores(message)
     return sentiment
